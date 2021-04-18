7aba83ab7b4f5c5b68f499a080660f2550bdaa81
Fung Cheng <lamfung.cheng@gmail.com>
2021-04-18 20:38:50 +0800

    add loading state UI

--- src/components/Charts/ChartWidget.vue
+++ src/components/Charts/ChartWidget.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="card">
+		<transition name="fade">
+			<div class="loading-state"  v-if="$apollo.loading"><span>Loading data...</span></div>
+		</transition>
 		<div class="card-body" v-if="hourDatas">
 			<div class="d-flex flex-column flex-lg-row justify-content-between">
 				<div>
@@ -114,3 +117,33 @@ export default {
 	},
 };
 </script>
+<style lang="scss" scoped>
+	.loading-state{
+		z-index: 999;
+		position: fixed;
+		background-color: rgba(0,0,0,0.5);
+		height: 100vh;
+		width: 100vw;
+		top:0px;
+		left:0px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		span{
+			background-color: white;
+			border-radius: 5px;
+			text-align: center;
+			width: 90%;
+			max-width: 400px;
+			max-height: 100px;
+			padding: 20px 10px;
+			font-size: 1.5rem;
+		}
+	}
+	.fade-enter-active, .fade-leave-active {
+		transition: opacity .5s;
+	}
+	.fade-enter, .fade-leave-to{
+		opacity: 0;
+	}
+</style>
