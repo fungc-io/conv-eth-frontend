7f3de832dd92842e972826857a51a116dca355af
Fung Cheng <lamfung.cheng@gmail.com>
2021-04-19 12:01:55 +0800

    bug fix: txn table pagination obstructed in small screens

--- src/components/Transactions/TransactionsSwap.vue
+++ src/components/Transactions/TransactionsSwap.vue
@@ -1,48 +1,50 @@
 <template>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Swaps</th>
-                    <th>Total Value</th>
-                    <th>Token Amount</th>
-                    <th>Token Amount</th>
-                    <th>Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="swap in swaps" :key="swap.id">
-                    <td>
-                        <a :href="`https://etherscan.io/tx/${swap.transaction.id}`" target="_blank">Swap
-                            {{ swap.amount0In != 0 ? "ETH for CONV" : "CONV for ETH" }}</a>
-                    </td>
-                    <td>{{ swap.amountUSD | USD }}</td>
-                    <td>
-                        <span v-if="swap.amount0In != 0">{{ swap.amount0In | ETH }}</span>
-                        <span v-else>{{ swap.amount1In | CONV }}</span>
-                    </td>
-                    <td>
-                        <span v-if="swap.amount0Out != 0">{{ swap.amount0Out | ETH }}</span>
-                        <span v-else>{{ swap.amount1Out | CONV }}</span>
-                    </td>
-                    <td>{{ swap.timestamp | fromNow }}</td>
-                </tr>
-            </tbody>
-        </table>
-        <nav aria-label="Pagination">
-            <ul class="pagination">
-                <li class="page-item" :class="{ disabled: !(page > 0) }">
-                    <a class="page-link" href="#" @click.prevent="prevPage"><span aria-hidden="true">&laquo;</span></a>
-                </li>
-                <li class="page-item">
-                    <span class="page-link">{{ page + 1 }}</span>
-                </li>
-                <li class="page-item" :class="{ disabled: !(page < maxPage) }">
-                    <a class="page-link" href="#" @click.prevent="nextPage"><span aria-hidden="true">&raquo;</span></a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+	<div>
+		<div class="table-responsive">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Swaps</th>
+						<th>Total Value</th>
+						<th>Token Amount</th>
+						<th>Token Amount</th>
+						<th>Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="swap in swaps" :key="swap.id">
+						<td>
+							<a :href="`https://etherscan.io/tx/${swap.transaction.id}`" target="_blank">Swap
+								{{ swap.amount0In != 0 ? "ETH for CONV" : "CONV for ETH" }}</a>
+						</td>
+						<td>{{ swap.amountUSD | USD }}</td>
+						<td>
+							<span v-if="swap.amount0In != 0">{{ swap.amount0In | ETH }}</span>
+							<span v-else>{{ swap.amount1In | CONV }}</span>
+						</td>
+						<td>
+							<span v-if="swap.amount0Out != 0">{{ swap.amount0Out | ETH }}</span>
+							<span v-else>{{ swap.amount1Out | CONV }}</span>
+						</td>
+						<td>{{ swap.timestamp | fromNow }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<nav aria-label="Pagination">
+			<ul class="pagination">
+				<li class="page-item" :class="{ disabled: !(page > 0) }">
+					<a class="page-link" href="#" @click.prevent="prevPage"><span aria-hidden="true">&laquo;</span></a>
+				</li>
+				<li class="page-item">
+					<span class="page-link">{{ page + 1 }}</span>
+				</li>
+				<li class="page-item" :class="{ disabled: !(page < maxPage) }">
+					<a class="page-link" href="#" @click.prevent="nextPage"><span aria-hidden="true">&raquo;</span></a>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </template>
 
 <script>
