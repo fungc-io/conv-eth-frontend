63aa76bf5e99e16e758a773b2e64442efd78abbe
Fung Cheng <lamfung.cheng@gmail.com>
2021-04-19 11:38:46 +0800

    improved filters case coverage

--- src/filters/index.js
+++ src/filters/index.js
@@ -3,18 +3,20 @@ import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 dayjs.extend(relativeTime)
 function currencyFormat(value){
+    if(isNaN(value)) return 0
     if (value>0 && value < 0.01) return "<0.01"
+    if (value<0 && value > -0.01) return "> -0.01"
     return Number(value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits:2})
 }
 const filters = {
     'USD': function(value){
-        return (value)?`US$${currencyFormat(value)}`:'0'
+        return (!isNaN(value))?`US$${currencyFormat(value)}`:'-'
     },
     'CONV': function(value){
-        return (value)?`${currencyFormat(value)} CONV`:'0'
+        return (!isNaN(value))?`${currencyFormat(value)} CONV`:'-'
     },
     'ETH': function(value){
-        return (value)?`${currencyFormat(value)} ETH`:'0'
+        return (!isNaN(value))?`${currencyFormat(value)} ETH`:'-'
     },
     'fromNow': function(timestamp){
         return dayjs(timestamp*1000).fromNow()
